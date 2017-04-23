3b5cdeeee4940b7e98f0bb6657b5a3d9b4247251
sr-sourabh <sourabhchaudhary012@gmail.com>
2017-04-23 22:53:19 +0530

    IC

---
+++ games/template.php
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://www.indianclashroyale.tk/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://www.w3schools.com/lib/w3data.js"></script>
+    <link href="http://www.indianclashroyale.tk/css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      
+      
+      
+      	
+        
+        
+        <div w3-include-html="nav.html"></div> 
+	<script>
+		w3IncludeHTML();
+	</script>
+					
+    
+
+    
+    
+    
+      
+      
+        <div w3-include-html="footer.html"></div> 
+	<script>
+		w3IncludeHTML();
+	</script>
+    </div>
+  </body>
+</html>
