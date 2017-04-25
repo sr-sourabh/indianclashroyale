b747de47838d2ebc15acc378f2dab833b2942bcc
Amith Mohanan <amithm7@gmail.com>
2017-04-25 09:51:10 +0530

    style: Code Quality 1. Removed trailing whitespaces 2. Converted indentations to 2 Spaces

--- addClan.php
+++ addClan.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8"> 
-		<title>Indian Clash Royale</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-		<div class="container-fluid">
-			<a href="index.php"><h1>Indian Clash Royale</h1></a>
-			<h2>Add Clan</h2>
-			<form id="add_clan" method="post" action="index.php">
-				<div class="form-group">
-				  <label for="name">Name</label>
-				  <input type="text" class="form-control" name="name" id="name">
-				</div>
-				<button type="submit" name="save" id="save" class="btn btn-primary btn-block">Save</button>
-			</form>
-		</div>
-	</body>
+  <head>
+    <meta charset="utf-8"> 
+    <title>Indian Clash Royale</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <a href="index.php"><h1>Indian Clash Royale</h1></a>
+      <h2>Add Clan</h2>
+      <form id="add_clan" method="post" action="index.php">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" name="name" id="name">
+        </div>
+        <button type="submit" name="save" id="save" class="btn btn-primary btn-block">Save</button>
+      </form>
+    </div>
+  </body>
 </html>
